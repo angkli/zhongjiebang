4ba00a9ddde1c022a4a9a690bbc1988ccfe705aa
ohoh <ohoh.co@qq.com>
2016-03-29 14:27:46 +0800

    演示

---
+++ app/src/main/java/com/zhongjiebang/android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhongjiebang.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
